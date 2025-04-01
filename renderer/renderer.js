69f1c13502b041b02279311b87db92b117fab7bb
André <coxtonhoxton@gmail.com>
2025-04-01 21:23:12 +0200

    Added Tab System with selector and basic layout for Settings Window

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,6 +1,7 @@
 const sidenavBtn    = document.getElementById('burgerMenu');
+const burgerSVG     = document.getElementById('burger');
 const sidenav       = document.getElementById('sidenav');
-const sidenavClose  = document.getElementById('closebtn'); 
+const tabItem       = document.querySelectorAll('.tab-item');
 
 
 
@@ -13,9 +14,24 @@ function initializeUI(){
 
 
 function setupEventListeners(){
+    
     sidenavBtn.addEventListener('click', () => sidenav.classList.toggle('active'));
-    sidenavClose.addEventListener('click', () => sidenav.classList.toggle('active'));
+    sidenavBtn.addEventListener('click', () => burgerSVG.classList.toggle('open'));
+
+    tabItemSelector();
 }
 
+function tabItemSelector(){
+    tabItem.forEach((tab) =>{
+        tab.addEventListener('click', function(e){
+            var target = e.currentTarget;
+
+            tabItem.forEach(t => t.classList.remove('selected'));
+
+            target.classList.add('selected');
+
+        })
+    })
+}
 
 window.addEventListener('DOMContentLoaded', initializeUI);
